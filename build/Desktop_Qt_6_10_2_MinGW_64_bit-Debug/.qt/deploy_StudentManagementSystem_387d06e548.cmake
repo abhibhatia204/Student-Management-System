037643747100cc8a5462e07a214255d775897dd5
Abhishek Bhatia <abhibhatia204@gmail.com>
2026-03-02 09:05:25 +0530

    Removed build folder and added .gitignore

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/deploy_StudentManagementSystem_387d06e548.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/Abhishek bhatia/Documents/StudentManagementSystem/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/StudentManagementSystem-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/Abhishek bhatia/Documents/StudentManagementSystem/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/StudentManagementSystem.exe"
-    GENERATE_QT_CONF
-)
